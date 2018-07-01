be0374b33086696d45b89ccdc0abaacc281f2381
Vivek Kartha <vivek.kartha@gmail.com>
2018-07-01 15:52:12 +0530

    Cleanup : Formatting

--- sum_pair.go
+++ sum_pair.go
@@ -3,34 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{1,2,3,4,1}
-	findPairs(a,5)
+	a := []int{1, 2, 3, 4, 1}
+	findPairs(a, 5)
 }
 
-func findPairs(a []int,sum int){
-	fmt.Println("Target sum =",sum)
+func findPairs(a []int, sum int) {
+	fmt.Println("Target sum =", sum)
 	var pairs [][] int
-	 m:= map[int] int{}
-	for i,v := range a{
-		complement := sum-v
-		if hasKey(m,complement){
-			index :=m[complement]
-			pairs = append(pairs,[]int{v,a[index]})
-		}else{
-			m[v]=i
+	m := map[int]int{}
+
+	for i, v := range a {
+		complement := sum - v
+		if hasKey(m, complement) {
+			index := m[complement]
+			pairs = append(pairs, []int{v, a[index]})
+		} else {
+			m[v] = i
 		}
 	}
 
 	printSlice(pairs)
 }
 
-func printSlice(pairs [][]int){
-	for _,v := range pairs{
+func printSlice(pairs [][]int) {
+	for _, v := range pairs {
 		fmt.Println(v)
 	}
 }
 
-func hasKey(m map[int]int,key int) bool {
-	_,ok := m[key]
+func hasKey(m map[int]int, key int) bool {
+	_, ok := m[key]
 	return ok
 }
